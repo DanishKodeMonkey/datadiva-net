2879e1c44b56bc1e68b644234c1728baf816543f
danishkodemonkey <humberjini@hotmail.com>
2023-07-05 18:42:56 +0200

    v2

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-function navigateToLayer(layerID) {
-  var layerContainer = document.getElementById('layerContainer');
+/*
+Cross layer/page functions defined below
+*/
 
-  // Clear the container and add the new iframe
-  layerContainer.innerHTML = '';
-  layerContainer.appendChild(createLayerIframe(layerID));
+//layer navigation
+function navigateToLayer(layer) {
+  const layerFrame = document.querySelector("#layerContainer iframe");
+  layerFrame.src = `layers/${layer}.html`;
 }
 
-  function createLayerIframe(layerID) {
-    var iframe = document.createElement('iframe');
-    iframe.src = 'layers/' + layerID + '.html'; // Adjust the path to your layer files
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.border = 'none';
-  
-    return iframe;
-  }
+//Function to handle Iframe paging
+function createLayerIframe(layerID) {
+  var iframe = document.createElement('iframe');
+  iframe.src = 'layers/' + layerID + '.html'; // Adjust the path to your layer files
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = 'none';
 
-  //button function for layer1
-  function grantAccess() {
-    var status = document.getElementById('status');
-    status.textContent = 'Access granted, welcome home DD';
+  return iframe;
+}
 
-    setTimeout(function(){
-      window.location.href = 'layer2.html';
-    },2000) //time delay before
-  }
+//Funciton to create a transition between layers
+function transitionToLayer(layer) {
+  var iframe = document.getElementById('layer-frame');
+  iframe.style.opacity = 0; // Fade out the iframe
+
+  setTimeout(function() {
+    // Load the new layer content
+    iframe.src = layer;
+
+    setTimeout(function() {
+      iframe.style.opacity = 1; // Fade in the iframe
+    }, 100); // Add a slight delay before fading in to ensure smooth transition
+  }, 300); // Wait for the fade-out transition duration
+}
